ca17ced19235f03b6b1e1b3357a46d56edaa9aee
Chris Dominic Chan <chanchrisdominic@gmail.com>
2023-08-03 21:06:01 +0800

    prioritize cached client mac in session

--- src/handlers/index.rs
+++ src/handlers/index.rs
@@ -1,6 +1,6 @@
 #![allow(warnings, unused)]
 use std::process::Command;
-    
+
 use axum::{
     body::Body,
     extract::State,
@@ -21,30 +21,27 @@ pub async fn index(
     TrueClientIp(ip): TrueClientIp,
     req: Request<Body>,
 ) -> Result<impl IntoResponse, StatusCode> {
+    let mac = session.get::<String>("mac");
+    let mac = match mac {
+        Some(mac) => mac,
+        None => {
+            let output = tokio::task::spawn_blocking(move || {
+                Command::new("ndsctl")
+                    .arg("json")
+                    .arg(ip.to_string())
+                    .output()
+                    .unwrap()
+            })
+            .await
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let output = tokio::task::spawn_blocking(move || {
-        Command::new("ndsctl")
-            .arg("json")
-            .arg(ip.to_string())
-            .output()
-            .unwrap()
-    })
-    .await
-    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+            let parsed_output = std::str::from_utf8(&output.stdout).unwrap();
+            let parsed_output = serde_json::from_str::<RawClientData>(parsed_output)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    let parsed_output = std::str::from_utf8(&output.stdout).unwrap();
+            session.insert("mac", &parsed_output.mac).unwrap();
 
-    let parsed_output = serde_json::from_str::<RawClientData>(parsed_output);
-    let mac = match parsed_output {
-        Ok(data) => data.mac,
-        Err(_) => {
-            let mac = session.get::<String>("mac");
-
-            if mac.is_some() {
-                mac.unwrap()
-            } else {
-                return Err(StatusCode::INTERNAL_SERVER_ERROR);
-            }
+            parsed_output.mac
         }
     };
 
@@ -65,69 +62,10 @@ pub async fn index(
         client.insert(&state.connection).await.unwrap();
     }
 
-    let mac_exists = session.get::<String>("mac").is_some();
-    if !mac_exists {
-        session.insert("mac", &mac).unwrap();
-    }
-
-    Ok(file::open("./sites/index.html".parse().unwrap(), &req).await)
+    Ok(file::simple_open("./sites/index.html".parse().unwrap()).await)
 }
 
 #[cfg(debug_assertions)]
-pub async fn index(
-    // mut session: WritableSession,
-    // State(state): State<AppState>,
-    // TrueClientIp(ip): TrueClientIp,
-    req: Request<Body>,
-) -> Result<impl IntoResponse, StatusCode> {
-    // let output = tokio::task::spawn_blocking(move || {
-    //     Command::new("ndsctl")
-    //         .arg("json")
-    //         .arg(ip.to_string())
-    //         .output()
-    //         .unwrap()
-    // })
-    // .await
-    // .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-
-    // let parsed_output = std::str::from_utf8(&output.stdout).unwrap();
-
-    // let parsed_output = serde_json::from_str::<RawClientData>(parsed_output);
-    // let mac = match parsed_output {
-    //     Ok(data) => data.mac,
-    //     Err(_) => {
-    //         let mac = session.get::<String>("mac");
-
-    //         if mac.is_some() {
-    //             mac.unwrap()
-    //         } else {
-    //             return Err(StatusCode::INTERNAL_SERVER_ERROR);
-    //         }
-    //     }
-    // };
-
-    // let possible_client = client::Entity::find()
-    //     .filter(client::Column::Mac.contains(&mac))
-    //     .one(&state.connection)
-    //     .await
-    //     .unwrap();
-
-    // if possible_client.is_none() {
-    //     let client = client::ActiveModel {
-    //         mac: ActiveValue::Set(mac.clone()),
-    //         credits: ActiveValue::Set(0),
-    //         remaining_seconds: ActiveValue::Set(0),
-    //         ..Default::default()
-    //     };
-
-    //     client.insert(&state.connection).await.unwrap();
-    // }
-
-    // let mac_exists = session.get::<String>("mac").is_some();
-    // if !mac_exists {
-    //     session.insert("mac", &mac).unwrap();
-    // }
-
+pub async fn index(req: Request<Body>) -> Result<impl IntoResponse, StatusCode> {
     Ok(file::open("./sites/index.html".parse().unwrap(), &req).await)
 }
-
