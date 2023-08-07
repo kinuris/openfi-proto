9875e8f5e081ded4cc597959ba1eeda53fcf6922
Chris Dominic Chan <chanchrisdominic@gmail.com>
2023-08-07 14:57:53 +0800

    added debug code

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
+use axum::http::Method;
 use axum::routing::post;
 use axum::{routing::get, Router};
 use axum_sessions::{async_session::MemoryStore, SessionLayer};
@@ -11,6 +12,7 @@ use tokio::sync::Mutex;
 use migration::{Migrator, MigratorTrait};
 use openfi_proto::{handlers, AppState, ClientConnections};
 use sea_orm::Database;
+use tower_http::cors::{Any, CorsLayer};
 use tower_http::services::ServeDir;
 
 #[tokio::main]
@@ -23,14 +25,17 @@ async fn main() {
 
     let active_clients: ClientConnections = Arc::new(Mutex::new(HashMap::new()));
 
-    // Separate thread
-    tokio::spawn(decrement_active_user(
-        connection.clone(),
-        active_clients.clone(),
-    ));
+    if cfg!(not(debug_assertions)) {
+        // Separate thread
 
-    // Separate thread
-    tokio::spawn(assure_auth_client_state(active_clients.clone()));
+        tokio::spawn(decrement_active_user(
+            connection.clone(),
+            active_clients.clone(),
+        ));
+        // Separate thread
+
+        tokio::spawn(assure_auth_client_state(active_clients.clone()));
+    }
 
     let session_layer = SessionLayer::new(
         MemoryStore::new(),
@@ -38,20 +43,35 @@ async fn main() {
     )
     .with_secure(false);
 
-    let app = Router::new()
+    let mut app = Router::new()
         .route("/", get(handlers::index::index))
-        .route("/status/", get(handlers::status::status))
+        .route("/admin", get(handlers::admin::admin))
+        .route("/status", get(handlers::status::status))
+        .route("/data", get(handlers::data::data))
+        .route("/data-stream", get(handlers::data::data_stream))
         .route(
-            "/request-access/",
+            "/request-access",
             post(handlers::request_access::request_access),
         )
-        .route("/pause/", post(handlers::pause::pause))
-        .nest_service("/assets/", ServeDir::new("./sites/assets"))
+        .route("/pause", post(handlers::pause::pause))
+        .route("/gen-code/:kind/:units", post(handlers::code::gen_code))
+        .route("/get-codes", post(handlers::code::get_codes))
+        .route("/redeem-code", post(handlers::code::redeem_code))
+        .nest_service("/index", ServeDir::new("./sites/index"))
         .with_state(AppState {
             active_clients,
             connection,
         })
         .layer(session_layer);
+
+    if cfg!(debug_assertions) {
+        let cors = CorsLayer::new()
+            .allow_methods([Method::GET, Method::POST])
+            .allow_headers(Any)
+            .allow_origin(Any);
+
+        app = app.layer(cors);
+    }
 
     axum::Server::bind(&"0.0.0.0:7766".parse().unwrap())
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
